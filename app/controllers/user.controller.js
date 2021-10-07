49a80f5d5ac3a5546fdfc599bcff90aa2ee9ab1b
mukund <mukund.s@intelaccounts.com>
2021-10-07 07:48:51 +0530

    user list api added

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,31 +1,36 @@
 const User = require("../models/user.model.js");
 // console.log(User);
 // Create and Save a new user
-exports.create = (req, res) => {
-  
+exports.create = (req, res) => {  
 };
 
 // Retrieve all users from the database.
 exports.findAll = (req, res) => {
-//   console.log('i callled now');
 };
 
 // Find a single user with a userId
-exports.findOne = (req, res) => {
-  
+exports.findOne = (req, res) => {  
 };
 
 // Update a user identified by the userId in the request
-exports.update = (req, res) => {
-  
+exports.update = (req, res) => {  
 };
 
 // Delete a user with the specified userId in the request
-exports.delete = (req, res) => {
-  
+exports.delete = (req, res) => {  
 };
 
 // Delete all users from the database.
-exports.deleteAll = (req, res) => {
-  
+exports.deleteAll = (req, res) => {  
 };
+
+exports.findAll = (req, res) => {
+    User.getAll((err, data) => {
+      if (err)
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while retrieving customers."
+        });
+      else res.send(data);
+    });
+  };
